daba1ff187d21d64a6285a8b50c4bf97d70b1f81
Maria <cascercerim@gmail.com>
2019-10-09 23:27:44 -0400

    Almost finished updated portfolio

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+// nav js
+const navSlide = () =>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links, li');
+
+    //toggle nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+         //animate lines
+        navLinks.forEach((link, index) => {
+            // console.log(index);
+            if (link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s';
+            }
+    });
+    // burger animation
+     burger.classList.toggle('toggle');
+});
+}
+navSlide();
